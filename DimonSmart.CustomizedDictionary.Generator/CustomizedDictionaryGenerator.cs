61c81962caecceadb3e9771b06bb44ac66961ff2
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-03-06 16:10:36 +0400

    Dictionary templace added

--- DimonSmart.CustomizedDictionary.Generator/CustomizedDictionaryGenerator.cs
+++ DimonSmart.CustomizedDictionary.Generator/CustomizedDictionaryGenerator.cs
@@ -1,15 +1,14 @@
-﻿using System.Diagnostics;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 using System.Text;
-using System;
+using static DimonSmart.CustomizedDictionary.Generator.CustomizedDictionaryTemplate;
 
 namespace DimonSmart.CustomizedDictionary.Generator
 {
     [Generator]
-    public class CustomerDictionaryGenerator : ISourceGenerator
+    public class CustomizedDictionaryGenerator : ISourceGenerator
     {
         public void Initialize(GeneratorInitializationContext context)
         {
@@ -20,7 +19,8 @@ namespace DimonSmart.CustomizedDictionary.Generator
         {
             var myFiles = context
                 .AdditionalFiles
-                .Where(at => at.Path.EndsWith(".csv"));
+                .Where(at => at.Path.EndsWith(".dictionary"));
+
             // if (!Debugger.IsAttached)
             //    Debugger.Launch();
 
@@ -28,34 +28,26 @@ namespace DimonSmart.CustomizedDictionary.Generator
             {
                 var dictionaryDescriptorSource = file.GetText(context.CancellationToken)?.ToString();
                 var customizedDictionarySpecification = new CustomizedDictionarySpecification(dictionaryDescriptorSource);
-                var generatedClass = GetGeneratedClass(customizedDictionarySpecification);
+                var generatedClass = GetGeneratedClass(DictionaryTemplate, customizedDictionarySpecification);
                 var sourceText = SourceText.From(generatedClass, Encoding.UTF8);
                 context.AddSource($"{Path.GetFileNameWithoutExtension(file.Path)}generated.cs", sourceText);
             }
         }
 
-        private string GetGeneratedClass(CustomizedDictionarySpecification customizedDictionarySpecification)
+        private static string GetGeneratedClass(string template, CustomizedDictionarySpecification customizedDictionarySpecification)
         {
-            var src = DictionaryTemplate;
+            var src = template;
+
             src = src.Replace("@@NameSpace", customizedDictionarySpecification.Namespace);
+            src = src.Replace("@@DictionaryName", customizedDictionarySpecification.DictionaryName);
+            src = src.Replace("@@KeyType", customizedDictionarySpecification.KeyType);
+            src = src.Replace("@@KeyName", customizedDictionarySpecification.KeyName);
+            src = src.Replace("@@KeyNamePlural", customizedDictionarySpecification.KeyNamePlural);
+            src = src.Replace("@@ValueType", customizedDictionarySpecification.ValueType);
+            src = src.Replace("@@ValueName", customizedDictionarySpecification.ValueName);
+            src = src.Replace("@@ValueNamePlural", customizedDictionarySpecification.ValueNamePlural);
 
             return src;
         }
-
-        private const string DictionaryTemplate =
-            """
-            namespace @@NameSpace;
-            public class CustomerDictionary
-            {
-                public void Print()
-                {
-                    Console.Writeline("7");
-                }
-            
-            }
-            
-            
-            
-            """;
     }
 }
---
+++ DimonSmart.CustomizedDictionary.Generator/CustomizedDictionaryTemplate.cs
@@ -0,0 +1,53 @@
+﻿namespace DimonSmart.CustomizedDictionary.Generator;
+
+public static class CustomizedDictionaryTemplate
+{
+    public static string DictionaryTemplate = @"
+namespace @@NameSpace
+{
+    public class @@DictionaryName
+    {
+        private Dictionary<@@KeyType, @@ValueType> _items = new Dictionary<@@KeyType, @@ValueType>();
+
+        public @@ValueType this[@@KeyType @@KeyName]
+        {
+            get => _items.TryGetValue(@@KeyName, out var value) ? value : default;
+            set => _items[@@KeyName] = value;
+        }
+
+        public void Add(@@KeyType @@KeyName, @@ValueType @@ValueName)
+        {
+            if (!_items.ContainsKey(@@KeyName))
+            {
+                _items.Add(@@KeyName, @@ValueName);
+            }
+            else
+            {
+                throw new ArgumentException($""An item with the same @@KeyName already exists."");
+            }
+        }
+
+        public bool ContainsKey(@@KeyType @@KeyName)
+        {
+            return _items.ContainsKey(@@KeyName);
+        }
+
+        public bool Remove(@@KeyType @@KeyName)
+        {
+            return _items.Remove(@@KeyName);
+        }
+
+        public int Count => _items.Count;
+
+        public IEnumerable<@@KeyType> @@KeyNamePlural => _items.Keys;
+
+        public IEnumerable<@@ValueType> @@ValueNamePlural => _items.Values;
+
+        public bool TryGetValue(@@KeyType @@KeyName, out @@ValueType @@ValueName)
+        {
+            return _items.TryGetValue(@@KeyName, out @@ValueName);
+        }
+    }
+}";
+
+}
