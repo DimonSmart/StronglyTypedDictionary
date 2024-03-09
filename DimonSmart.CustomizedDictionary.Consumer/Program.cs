fd6572ab0cec3b24a345f4811b2c8bd669a42f88
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-03-09 10:12:22 +0400

    Code cleanup

--- DimonSmart.CustomizedDictionary.Consumer/Program.cs
+++ DimonSmart.CustomizedDictionary.Consumer/Program.cs
@@ -4,15 +4,15 @@ public static class Program
 {
     public static void Main()
     {
-      var d = new ErrorNumberToMessageMapping();
-      d.Add(404, "Something not found");
-      if (!d.ContainsErrorCode(42))
-          Console.WriteLine("May be this is not an error at all");
+        var d = new ErrorNumberToMessageMapping();
+        d.Add(404, "Something not found");
+        if (!d.ContainsErrorCode(42))
+            Console.WriteLine("May be this is not an error at all");
 
-      if (d.TryGetMessage(404, out var errorMessage))
-          Console.WriteLine($"404: {errorMessage}");
+        if (d.TryGetMessage(404, out var errorMessage))
+            Console.WriteLine($"404: {errorMessage}");
 
-              // d.GetMessageOrDefault(43);
+        // d.GetMessageOrDefault(43);
 
     }
 }
